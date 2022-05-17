a6ff0c4b4c1c6c06a88e0cdc192c07107908ba88
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-17 11:46:32 -0300

    H2 database

---
+++ backend/src/main/java/com/tadelle/dscatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.tadelle.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tadelle.dscatalog.entities.Category;
+import com.tadelle.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+}
