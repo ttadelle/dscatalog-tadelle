ebab258fa69c203bf35b18be786b87f82ddb682e
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-18 20:02:47 -0300

    DTO

--- backend/src/main/java/com/tadelle/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/tadelle/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.tadelle.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tadelle.dscatalog.dto.CategoryDTO;
 import com.tadelle.dscatalog.entities.Category;
 import com.tadelle.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly=true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 
 }
