a3a16431f1ab1541b9e7428451fb6f09270866d3
Thiago Tadelle <thiago.tadelle@gmail.com>
2022-05-26 18:38:17 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/tadelle/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.tadelle.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tadelle.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}	
