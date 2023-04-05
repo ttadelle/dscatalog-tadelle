bde942154c3fc1ca91b81168a003924b8f952271
Thiago Tadelle <thiago.tadelle@gmail.com>
2023-04-04 22:44:16 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,15 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 
-function App() {
+const App = () => {
   return (
+    <>
+    <Navbar />
     <h1>Hello DSCatalog</h1>
+    </>
+    
   );
 }
 
